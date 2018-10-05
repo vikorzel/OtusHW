695fd987efefc563dcdeefb3a572f3dca1fdd151
Alexander Mitichev <Mitichev.A@crosstech.su>
2018-10-05 17:35:59 +0300

    warnings fixed output name changed gitignore updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "print_funcs.h"
 #include <iostream>
-int main(int argc, char** argv){
+int main(){
 	std::cout << "build " << GetVersion() << std::endl;
 	std::cout<<GetHelloWorld();
 	return 0;
